c18945068f3a7965641791fc4b000e1c8303dfe8
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-09-15 20:41:08 +0800

    Support leading and trailing iconify icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,13 +4,25 @@ type Props = {
   icon: string;
   inline?: boolean;
   color?: string;
+  leading?: boolean;
+  trailing?: boolean;
 };
 
-const Icon: FC<Props> = ({ icon, inline = false, color }) => {
+const Icon: FC<Props> = ({
+  icon,
+  inline = false,
+  color,
+  leading = false,
+  trailing = false,
+}) => {
   const props = {
     icon,
     inline: inline || undefined,
-    style: color ? { color: color } : undefined,
+    style: {
+      color: color || undefined,
+      marginLeft: leading ? "-0.25em" : undefined,
+      marginRight: trailing ? "-0.25em" : undefined,
+    },
   };
   return <iconify-icon {...props}></iconify-icon>;
 };

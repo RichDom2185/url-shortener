ceef483662637c948a453a012f8b166f502b34d2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-09-18 14:00:08 +0800

    Improve NavBar
    * Add link to GitHub repository
    * Make navbar sticky to top of page
    * Add dynamic border to navbar based on scroll position

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,30 @@
+import { html } from "hono/html";
 import { FC } from "hono/jsx";
-import { SITE_TITLE } from "../utils/constants";
+import { SITE_REPOSITORY, SITE_TITLE } from "../utils/constants";
 import Icon from "./Icon";
 
 const NavBar: FC = () => {
   return (
-    <nav class="container-fluid">
+    <nav
+      id="site-nav"
+      class="container-fluid"
+      style={{
+        position: "sticky",
+        top: 0,
+        backgroundColor: "var(--background-color)",
+      }}
+    >
+      {html`<script>
+        window.addEventListener("scroll", () => {
+          const nav = document.getElementById("site-nav");
+          if (nav) {
+            nav.style.borderBottom =
+              window.scrollY > 0
+                ? "1px solid var(--muted-border-color)"
+                : "none";
+          }
+        });
+      </script>`}
       <ul>
         <li>
           <a href="/" class="contrast">
@@ -15,6 +35,23 @@ const NavBar: FC = () => {
             />
             &nbsp;
             <strong>{SITE_TITLE}</strong>
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a
+            href={SITE_REPOSITORY}
+            class="contrast"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub"
+            data-tooltip="GitHub"
+            data-placement="left"
+          >
+            {/* Inline even though no text, to line up with
+                other navbar components */}
+            <Icon inline icon="octicon:mark-github-16" />
           </a>
         </li>
       </ul>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = "URL Shortener" as const;
+export const SITE_REPOSITORY =
+  "https://github.com/RichDom2185/url-shortener" as const;
 export const AUTHOR_SITE = "https://richdom2185.github.io" as const;
